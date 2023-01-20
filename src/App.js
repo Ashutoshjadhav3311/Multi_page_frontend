ab7ff94276b4fb89a698b9a8a24444d26a7ed4d3
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2023-01-20 13:09:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navigation, Footer, Home, About, Contact } from "./components";
-import App from "./App";
-import VC from "./VC";
+import React, { useState, createContext } from "react";
+import Container from "./Container";
+import "./App.css";
 
-ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/App" element={<App />} />
-      <Route path="/VC" element={<VC />} />
-    </Routes>
-    <Footer />
-  </Router>,
-
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
+const ConnectionContext = createContext({
+  connection: null,
+  updateConnection: () => {},
+});
+const ChannelContext = createContext({
+  channel: null,
+  updateChannel: () => {},
+});
+const App = () => {
+  const [connection, setconnection] = useState(null);
+  const [channel, setChannel] = useState(null);
+  const updateConnection = conn => {
+    setconnection(conn);
+  };
+  const updateChannel = chn => {
+    setChannel(chn);
+  };
+  return (
+    <ConnectionContext.Provider value={{ connection, updateConnection }}>
+      <ChannelContext.Provider value={{ channel, updateChannel }}>
+        <Container />
+      </ChannelContext.Provider>
+    </ConnectionContext.Provider>
+  );
+};
+export const ConnectionConsumer = ConnectionContext.Consumer;
+export const ChannelConsumer = ChannelContext.Consumer;
+export default App;
