91e652245692cde8e730edba434b8a0965a534ae
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2023-01-21 18:43:02 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,12 +6,8 @@ function Contact() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-            <img
-              class="ui small circular image"
-              src="https://media.licdn.com/dms/image/C4E03AQFPtYZCdiznWA/profile-displayphoto-shrink_800_800/0/1643207133086?e=1679529600&v=beta&t=t8wxoaWHzy8qZO6Yz-Br-yMOj2ooLHUtCAp_WCx5uNA"
-              alt=""
-            />
-            <h3>Ashutosh Jadhav</h3>
+            
+            <h3></h3>
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Contact</h1>
